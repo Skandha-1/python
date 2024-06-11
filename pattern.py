c5c518db1ffdda48a7d9aa24d1aa4ac597454b93
Skandha-1 <161051420+Skandha-1@users.noreply.github.com>
2024-06-11 08:37:27 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,15 @@
+def print_star_pattern():
+    pattern = [
+        "*        ",
+        "  *    *  ",
+        "    *    ",
+        "  *    *  ",
+        "*        "
+    ]
+
+    for line in pattern:
+        print(line)
+
+
+# Call the function to print the pattern
+print_star_pattern()
